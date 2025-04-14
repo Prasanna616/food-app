34e8687bb3513c96b567f9ec16585352fdd8fbad
Pknashi <pknashi616@gmail.com>
2025-04-14 23:07:52 +0530

    added pkweb allowedhost

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--byxuz^ibw=rdzqeic^k5juuxt2l%_sjs$gaw4^o3-e227mzyk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pkweb.pythonanywhere.com']
 
 
 # Application definition
